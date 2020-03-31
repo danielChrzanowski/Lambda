97f81cfc9658c970489297e8c247f65a6f2cd4ca
Daniel <daniel_chrzanowski98@wp.pl>
2020-03-31 14:10:43 +0200

    changed look

--- src/main/java/flatMap/FlatMap.java
+++ src/main/java/flatMap/FlatMap.java
@@ -6,21 +6,20 @@ import java.util.stream.Collectors;
 
 public class FlatMap {
     public static void main(String[] args) {
-        //todo 1 utworz 3 listy intow
-        //todo 2 utworz liste list i wydrukuj
-        //todo 3 splaszcz liste uzywajac flatmap, posortuj i wydrukuj
+        //done 1 utworz 3 listy intow
+        //done 2 utworz liste list i wydrukuj
+        //done 3 splaszcz liste uzywajac flatmap, posortuj i wydrukuj
 
         List<Integer> pierwsze = List.of(5, 7, 11);
         List<Integer> parzyste = List.of(2, 4, 16);
         List<Integer> nieParzyste = List.of(1, 3, 5);
 
         List<List<Integer>> lista = List.of(pierwsze, parzyste, nieParzyste);
-
         System.out.println(lista);
 
         var lista2 = lista
                 .stream()
-                .flatMap(x -> x.stream())
+                .flatMap(m -> m.stream())
                 .collect(Collectors.toList());
         System.out.println(lista2);
 
