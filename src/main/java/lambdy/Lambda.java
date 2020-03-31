6f22c3e19bb3835fadb0247dff4b8c756d51f700
Daniel <daniel_chrzanowski98@wp.pl>
2020-03-31 09:55:01 +0200

    lambda, pom.xml

---
+++ src/main/java/lambdy/Lambda.java
@@ -0,0 +1,30 @@
+package lambdy;
+
+public class Lambda {
+    public static void main(String[] args) {
+        MyInterface impl1 = new KlasycznaImplementacja();
+
+        MyInterface impl2 = name -> name.toUpperCase();
+
+        System.out.println(impl1);
+        System.out.println(impl2);
+    }
+
+
+}
+
+@FunctionalInterface
+interface MyInterface {
+    String go(String name);
+}
+
+class KlasycznaImplementacja implements MyInterface {
+    @Override
+    public String go(String name) {
+        return name;
+    }
+}
+
+//done 1 void go();                   - interfejs, klasyczna implementacja, klasa anonimowa, lambda
+//todo 2 String go();                 - interfejs, klasyczna implementacja, klasa anonimowa, lambda
+//todo 3 String go(String name);      - interfejs, klasyczna implementacja, klasa anonimowa, lambda
